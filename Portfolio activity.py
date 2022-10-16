a3317917f4a04afc4b9b410b41126e1b45901052
Bhavay Upadhyay <105224920+bhavay2001@users.noreply.github.com>
2022-10-16 13:21:35 +0530

    Python portfolio Activity
    Both questions plotted and uploaded

---
+++ Portfolio activity.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Python Portfolio Activity (Bhavya Upadhyay-010)
+
+# Q1)Cubes:A number raised to the third power is a cube. Plot the first five cubic numbers, and then plot the first 5000 cubic numbers.
+
+# In[3]:
+
+
+#Bhavya Upadhyay-010
+#PLOTTING 5 CUBES
+from matplotlib import pyplot as plt
+
+# Define data.
+x_values = [1, 2, 3, 4, 5]
+cubes = [1, 8, 27, 64, 125]
+
+# Make plot.
+plt.scatter(x_values, cubes, edgecolor='none', s=40)
+
+# Customize plot.
+plt.title("Cubes", fontsize=24)
+plt.xlabel('Value', fontsize=14)
+plt.ylabel('Cube of Value', fontsize=14)
+plt.tick_params(axis='both', labelsize=14)
+
+# Show plot.
+plt.show()
+
+
+# PLOTTING 5000 CUBES
+
+# In[4]:
+
+
+from matplotlib import pyplot as plt
+
+# Define data.
+x_values = list(range(5001))
+cubes = [x**3 for x in x_values]
+
+# Make plot.
+plt.scatter(x_values, cubes, edgecolor='none', s=40)
+
+# Customize plot.
+plt.title("Cubes", fontsize=24)
+plt.xlabel('Value', fontsize=14)
+plt.ylabel('Cube of Value', fontsize=14)
+plt.tick_params(axis='both', labelsize=14)
+plt.axis([0, 5100, 0, 5100**3])
+
+# Show plot.
+plt.show()
+
+
+# Q2) Colored cubes:Apply a colormap to your cubes plot.
+
+# In[5]:
+
+
+from matplotlib import pyplot as plt
+
+# Define data.
+x_values = list(range(5001))
+cubes = [x**3 for x in x_values]
+
+# Make plot.
+plt.scatter(x_values, cubes, edgecolor='none', c=cubes, cmap=plt.cm.BuGn, s=40)
+
+# Customize plot.
+plt.title("Cubes", fontsize=24)
+plt.xlabel('Value', fontsize=14)
+plt.ylabel('Cube of Value', fontsize=14)
+plt.tick_params(axis='both', labelsize=14)
+plt.axis([0, 5100, 0, 5100**3])
+
+# Show plot.
+plt.show()
+
+
+# In[ ]:
+
+
+
+
